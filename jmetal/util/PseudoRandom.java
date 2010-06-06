1630cc4df1854c4df124993545b8f133d218364d
Matteo Miraz <matteo.miraz@gmail.com>
2010-06-06 10:29:11 +0200

    PseudoRandom is more flexible, and allows one to set or retrieve the seed being used

--- jmetal/util/PseudoRandom.java
+++ jmetal/util/PseudoRandom.java
@@ -10,84 +10,105 @@ package jmetal.util;
 import ec.util.MersenneTwisterFast;
 
 /**
- * Class representing some randoms facilities
+ * Manages random number generators
  */
 public abstract class PseudoRandom {
-  
-	private static final MersenneTwisterFast MT;
-	
-  /** generator used to obtain the random values */
-  private static final RandomGenerator random;
- 
-  /** other generator used to obtain the random values */
-  private static final java.util.Random randomJava;
-    
-  static {
-      long seed = System.currentTimeMillis();
-      //long seed = 13;
-  	System.out.println("Using seed: " + seed);
 
-  	// configuration 1: use mersenne twister fast
-  	MT = new MersenneTwisterFast(seed);
-  	random = null;
-  	randomJava = null;
-  	
-  	// configuration 2: use RandomGenerator & java.util.Random
-  	// MT = null;
-  	//random = new RandomGenerator();
-  	//randomJava = new java.util.Random();
-  }
-  
-  
+	/** the seed being used to generate numbers */
+	private static long seed;
+	/**
+	 * Returns the seed being used to generate numbers
+	 * @return the seed being used to generate numbers
+	 */
+	public static long getSeed() {
+		return seed;
+	}
+
+	/** Mersenne Twister Fast random number generator */
+	private static MersenneTwisterFast MT;
+
+	/**
+	 * Use Mersenne Twister to generate random numbers (and disable other random number generators)
+	 * @param seed_ the seed to use
+	 */
+	public static void setupMersenneTwisterFast(long seed_) {
+		seed = seed_;
+		MT = new MersenneTwisterFast(seed);
+		randomJava = null;
+	}
+
+
+	/** java random number generator */
+	private static java.util.Random randomJava;
+
+	/**
+	 * Use the java's Random class to generate random numbers (and disable other random number generators)
+	 * @param seed_ the seed to use
+	 */
+	public static void setupJavaRandom(long seed_) {
+		seed = seed_;
+		MT = null;
+		randomJava = new java.util.Random(seed);
+	}
+
+	static {
+		setupMersenneTwisterFast(System.currentTimeMillis());
+	}
+
 	public static MersenneTwisterFast getMersenneTwisterFast() {
 		return MT;
 	}
-  
-  /** 
-   * Returns a random int value using the Java random generator.
-   * @return A random int value.
-   */
-  public static int randInt() {
-  	if(MT != null) return MT.nextInt();
-  	
-    return randomJava.nextInt();
-  } // randInt
-    
-  /** 
-   * Returns a random double value using the PseudoRandom generator.
-   * Returns A random double value.
-   */
-  public static double randDouble() {
-  	if(MT != null) return MT.nextDouble();
-  	
-    return random.rndreal(0.0,1.0);
-    //return randomJava.nextDouble();
-  } // randDouble
-    
-  /** 
-   * Returns a random int value between a minimum bound and maximum bound using
-   * the PseudoRandom generator.
-   * @param minBound The minimum bound.
-   * @param maxBound The maximum bound.
-   * Return A pseudo random int value between minBound and maxBound.
-   */
-  public static int randInt(int minBound, int maxBound) {
-  	if(MT != null) return minBound + MT.nextInt(maxBound-minBound+1);
-  	
-  	return random.rnd(minBound,maxBound);
-    //return minBound + randomJava.nextInt(maxBound-minBound+1);
-  } // randInt
-    
-  /** Returns a random double value between a minimum bound and a maximum bound
-   * using the PseudoRandom generator.
-   * @param minBound The minimum bound.
-   * @param maxBound The maximum bound.
-   * @return A pseudo random double value between minBound and maxBound
-   */
-  public static double randDouble(double minBound, double maxBound) {
-  	if(MT != null) return minBound + (maxBound - minBound)*randomJava.nextDouble();
-  	
-    return random.rndreal(minBound,maxBound);
-    //return minBound + (maxBound - minBound)*randomJava.nextDouble();
-  } // randDouble    
+
+	/**
+	 * Returns a random int value using the Java random generator.
+	 * @return A random int value.
+	 */
+	public static int randInt() {
+		if (MT != null)
+			return MT.nextInt();
+
+		return randomJava.nextInt();
+	} // randInt
+
+	/**
+	 * Returns a random double value using the PseudoRandom generator.
+	 * @return A random double value.
+	 */
+	public static double randDouble() {
+		if (MT != null)
+			return MT.nextDouble();
+
+		return randomJava.nextDouble();
+	} // randDouble
+
+	/**
+	 * Returns a random int value between a minimum bound and maximum bound
+	 * using the PseudoRandom generator.
+	 *
+	 * @param minBound The minimum bound.
+	 * @param maxBound The maximum bound.
+	 * @return A pseudo random int value between minBound and maxBound.
+	 */
+	public static int randInt(int minBound, int maxBound) {
+		if (MT != null)
+			return minBound + MT.nextInt(maxBound - minBound + 1);
+
+		 return minBound + randomJava.nextInt(maxBound-minBound+1);
+	} // randInt
+
+	/**
+	 * Returns a random double value between a minimum bound and a maximum bound
+	 * using the PseudoRandom generator.
+	 *
+	 * @param minBound The minimum bound.
+	 * @param maxBound The maximum bound.
+	 * @return A pseudo random double value between minBound and maxBound
+	 */
+	public static double randDouble(double minBound, double maxBound) {
+		if (MT != null)
+			return minBound + (maxBound - minBound) * MT.nextDouble();
+
+		return minBound + (maxBound - minBound)*randomJava.nextDouble();
+	} // randDouble
+
 } // PseudoRandom
